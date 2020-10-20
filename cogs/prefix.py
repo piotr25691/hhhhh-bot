15f0bca6127f1df557cce0cb6fe6e71bf91b3628
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2020-10-20 09:33:13 +0200

    Add files via upload

---
+++ cogs/prefix.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import json
+from main import prefixes
+
+class prefix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="prefix")
+    @commands.has_permissions(manage_guild=True)
+    async def _prefix(self, ctx, *, body):
+        if body.endswith("|"):
+            prefixes[f'{ctx.message.guild.id}'] = body.replace("|", " ")
+        else:
+            prefixes[f'{ctx.message.guild.id}'] = body
+        with open("prefixes.json", "w+") as f:
+            json.dump(prefixes, f)
+        await ctx.send(f":white_check_mark: Set prefix for this guild as: `{body.replace('|', ' ')}`")
+
+def setup(bot):
+    bot.add_cog(prefix(bot))
+
--- main.py
+++ main.py
@@ -2,42 +2,39 @@
 
 # discord api
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 from discord.ext.tasks import loop
 
 # async functions & sleep
 import asyncio
 
-# regex (g-detector)
-import re
-
-
-# eval
-import inspect
-import io
-import textwrap
-import traceback
-from contextlib import redirect_stdout
-
-# config
+# config and prefix
 import json
 
-# nitro command
-import random
 # 24/7
 from webserver import keep_alive
-# math command
-import math
-# datestamp
-import datetime
-import itertools
-import os
-import secrets
+
+# variables
 from vars import *
 
+import math
+import secrets
+import re
 
-bot = commands.Bot(command_prefix='hh!')
+with open("prefixes.json") as f:
+    prefixes = json.load(f)
+default_prefix = "hh!"
+
+
+def prefix(bot, message):
+    id = message.guild.id
+    try:
+        return prefixes[f'{id}']
+    except KeyError:
+        return default_prefix
+
+
+bot = commands.Bot(command_prefix=prefix)
 bot.remove_command('help')
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
@@ -56,7 +53,7 @@ async def change_presence():
     await bot.change_presence(status='dnd', activity=activity)
     await asyncio.sleep(600)
 
-class hhhhh(commands.Cog):
+class maincog(commands.Cog):
 
     # initialize bot status and print bot is ready
     # error handler
@@ -107,62 +104,56 @@ class hhhhh(commands.Cog):
     @bot.event
     async def on_message(message):
 
-         msg = message.content
-         if message.author.bot:
+        msg = message.content
+        if message.author.bot:
             if message.author.id == 759674875605680158:
                 pass
             else:
                 return
 
-         if "```http" in message.content:
-             if message.channel.id == 759680439778148405:
-                return await message.channel.send(f":gear: Keep-Online:tm: packet registered\nPacket ID: {secrets.token_hex(16)}")
-             else:
+        if "```http" in message.content:
+            if message.channel.id == 759680439778148405:
+                return await message.channel.send(
+                    f":gear: Keep-Online:tm: packet registered\nPacket ID: {secrets.token_hex(16)}")
+            else:
                 pass
 
-         if message.content.casefold() in map(str.casefold, forbidden):
-             await message.delete()
-             h = await message.channel.send("no")
-             await asyncio.sleep(2)
-             await h.delete()
+        if message.content.casefold() in map(str.casefold, forbidden):
+            await message.delete()
+            h = await message.channel.send("no")
+            await asyncio.sleep(2)
+            await h.delete()
 
-         hresult = re.compile(r'\bh+\b', re.IGNORECASE).findall(message.content)
+        hresult = re.compile(r'\bh+\b', re.IGNORECASE).findall(message.content)
 
-         if hresult:
-             if "hh!" in message.content:
-                 pass
-             else:
+        if hresult:
+            if "hh!" in message.content:
+                pass
+            else:
                 global hcount
                 hcount = hcount + 1
                 with open("hcount.txt", "w") as f:
                     f.write(str(hcount))
                     f.close()
 
-         if "@someone" in message.content:
-             member = random.choice(message.guild.members)
-             while member.bot == True:
-                member = random.choice(message.guild.members)
-                if member.bot == False:
-                    break
-             await message.channel.send("<@!" + str(member.id) + ">")
-
-         if "gatos" in message.content:
+        if "gatos" in message.content:
             await message.channel.purge(limit=5)
 
-         # if any of the regexes match do the following
-         if message.content in pings:
-             embedVar = discord.Embed(title=":information_source: Notice", description="Do you want my prefix? Just use `hh!`", color=0x7289da)
-             embedVar.set_thumbnail(url="https://cdn.discordapp.com/avatars/742388119516741642/0547c1220f0ed953aee67751730d37e0.webp?size=1024")
-             return await message.channel.send(embed=embedVar)
+        # if any of the regexes match do the following
+        if message.content in pings:
+            embedVar = discord.Embed(title=":information_source: Notice",
+                                     description="Do you want my prefix? Just use `hh!`", color=0x7289da)
+            embedVar.set_thumbnail(
+                url="https://cdn.discordapp.com/avatars/742388119516741642/0547c1220f0ed953aee67751730d37e0.webp?size=1024")
+            return await message.channel.send(embed=embedVar)
         # allow the bot to process commands
-        
-         await bot.process_commands(message)
+
+        await bot.process_commands(message)
 
 
 @bot.event
 async def on_ready():
     change_presence.start()
-    bot.add_cog(hhhhh(bot))
     activity = discord.Activity(type=discord.ActivityType.streaming, name=f"h | Use hh!help | {version}",
                                 url="https://www.youtube.com/watch?v=DwjbP7aihxQ")
     await bot.change_presence(status='dnd', activity=activity)
