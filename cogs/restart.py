ffcc3c46976e6f432f8255fba307d450248810af
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2020-10-21 14:09:39 +0200

    Add files via upload

--- cogs/restart.py
+++ cogs/restart.py
@@ -2,19 +2,23 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import sys
+import os
 
 
 class stop(commands.Cog):
     # stop command
-    @commands.command(aliases=['kill', 'die'])
+    @commands.command(aliases=['restart', 'reboot'])
     # check whether the bot owner tries to kill the bot from chat or not
     @commands.is_owner()
-    async def terminate(self, ctx):
+    async def restart_(self, ctx):
         # delete the message
         await ctx.message.delete()
         # stop execution
-        await ctx.send(':white_check_mark: Stopped <@!742388119516741642>')
-        sys.exit('bot has been stopped by bot owner')
+        await ctx.send(':gear: Restarting...')
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+        await ctx.send(':white_check_mark: Restarted <@!742388119516741642>')
+        sys.exit('bot has been restarted by bot owner')
 
 def setup(bot):
     bot.add_cog(stop(bot))
