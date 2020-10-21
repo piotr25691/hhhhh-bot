7d34b23a802c5371592cfa60a49831233851b728
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2020-10-21 14:19:23 +0200

    Update restart.py

--- cogs/restart.py
+++ cogs/restart.py
@@ -6,7 +6,7 @@ import os
 import asyncio
 
 
-class stop(commands.Cog):
+class restart(commands.Cog):
     # stop command
     @commands.command(aliases=['restart', 'reboot'])
     # check whether the bot owner tries to kill the bot from chat or not
@@ -23,4 +23,4 @@ class stop(commands.Cog):
         sys.exit('bot has been restarted by bot owner')
 
 def setup(bot):
-    bot.add_cog(stop(bot))
+    bot.add_cog(restart(bot))
