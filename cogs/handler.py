12aa86b7693ba5940fe0c3c37fbd4ed7e9d8d2cc
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2020-10-22 15:12:52 +0200

    Update handler.py

--- cogs/handler.py
+++ cogs/handler.py
@@ -13,8 +13,9 @@ class handler(commands.Cog):
             prefix = "hh!"
         if maintenance == False:
             if message.content in pings:
-                embedVar = discord.Embed(title=":information_source: Notice",
+                embedVar = discord.Embed(
                                      description=f"Do you want my prefix? Just use `{prefix}`", color=0x7289da)
+                embedVar.set_author(name='Notice', icon_url="https://i.imgur.com/A8g1ViW.png")
                 embedVar.set_thumbnail(
                 url="https://cdn.discordapp.com/avatars/742388119516741642/0547c1220f0ed953aee67751730d37e0.webp?size=1024")
                 return await message.channel.send(embed=embedVar)
@@ -30,38 +31,44 @@ class handler(commands.Cog):
         # bot - nonexistent command
         if isinstance(error, commands.CommandNotFound):
             await ctx.message.delete()
-            embedVar = discord.Embed(title=":x: Command Not Found", description="This command does not exist.",
+            embedVar = discord.Embed(description="This command does not exist.",
                                          color=0xff0000)
+            embedVar.set_author(name='Command Not Found', icon_url="https://i.imgur.com/OyDaCvd.png")
             return await ctx.send(embed=embedVar, delete_after=10)
         # bot - command disabled
         if isinstance(error, commands.DisabledCommand):
             userid = ctx.message.author.id
-            embedVar = discord.Embed(title=":x: Disabled", description="This command has been disabled.",
+            embedVar = discord.Embed(description="This command has been disabled.",
                                      color=0xff0000)
+            embedVar.set_author(name='Disabled', icon_url="https://i.imgur.com/OyDaCvd.png")
             return await ctx.send(embed=embedVar, delete_after=10)
         # bot - command on cooldown
         if isinstance(error, commands.CommandOnCooldown):
-            embedVar = discord.Embed(title=":x: Ratelimited",
+            embedVar = discord.Embed(
                                      description=f"You are ratelimited. Please try aqain in {math.ceil(error.retry_after)} seconds.",
                                      color=0xff0000)
+            embedVar.set_author(name='Ratelimited', icon_url="https://i.imgur.com/OyDaCvd.png")
             return await ctx.send(embed=embedVar, delete_after=10)
         # user - missing permissions
         if isinstance(error, commands.MissingPermissions):
-            embedVar = discord.Embed(title=":x: Error", description=f"You can't use this command.", color=0xff0000)
+            embedVar = discord.Embed(description=f"You can't use this command.", color=0xff0000)
+            embedVar.set_author(name='Error', icon_url="https://i.imgur.com/OyDaCvd.png")
             return await ctx.send(embed=embedVar, delete_after=10)
         # user - no DM
         if isinstance(error, commands.NoPrivateMessage):
             userid = ctx.message.author.id
             try:
-                embedVar = discord.Embed(title=":x: Error", description=f"You can't use this command in DMs.",
+                embedVar = discord.Embed(description=f"You can't use this command in DMs.",
                                          color=0xff0000)
+                embedVar.set_author(name='Error', icon_url="https://i.imgur.com/OyDaCvd.png")
                 return await ctx.author.send(embed=embedVar, delete_after=10)
             except discord.Forbidden:
                 pass
                 return
         # user - no permission
         if isinstance(error, commands.CheckFailure):
-            embedVar = discord.Embed(title=":x: Error", description=f"You can't use this command.", color=0xff0000)
+            embedVar = discord.Embed(description=f"You can't use this command.", color=0xff0000)
+            embedVar.set_author(name='Error', icon_url="https://i.imgur.com/OyDaCvd.png")
             return await ctx.send(embed=embedVar, delete_after=10)
         
 
