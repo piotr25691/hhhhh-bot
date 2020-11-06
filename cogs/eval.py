c868ad609a2c0c31248053563eb09da10337a78c
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2020-11-06 19:28:59 +0100

    Update eval.py

--- cogs/eval.py
+++ cogs/eval.py
@@ -4,10 +4,10 @@ from discord.ext.commands.errors import CheckFailure
 import inspect
 import io
 import textwrap
-import ast
 import traceback
 import sys
-import json
+import time
+import math
 from contextlib import redirect_stdout
 admins = [603635602809946113, 444550944110149633, 429935667737264139, 350325552344858624]
 token_msgs = ["hh!eval token", "hh!eval TOKEN", "hh!e token", "hh!e TOKEN", "hh!evaluate token", "hh!evaluate TOKEN"]
@@ -18,10 +18,21 @@ class _eval(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+      if message.author.id == 742388119516741642:
+        global message_c
+        if not message.content.startswith("```py\n"):
+          message_c = message.content
+        else:
+          message_c = message.content.replace("```py\n", "")
+          message_c = message_c.replace("\n```", "")
+
     @commands.command(aliases=['e', 'eval', 'evaluate'])
     # check whether the bot owner executes the command or not
     async def _eval(self, ctx, *, body):
-        if ctx.author.id in admins:
+        if ctx.author.id in admins: 
+            start = time.time()
             await ctx.message.delete()
             # evaluate and execute python code
             env = {
@@ -35,18 +46,17 @@ class _eval(commands.Cog):
                 'TOKEN': token,
                 'version': sys.version
             }
-            if ctx.message.content in token_msgs:
-                return await ctx.send(token)
-
             if "config.json" in ctx.message.content:
-                err = await ctx.send(":no_entry: **NO!** I'm not leaking my token!")
-                return
+                embedVar = discord.Embed(title=":x: An exception has occurred", description="```py\nYou aren't allowed to leak my token! ```", color=0xff0000)
+                embedVar.set_footer(text="TokenLeakError")
+                return await ctx.send(embed=embedVar)
 
             for word in tos:
                 if word in ctx.message.content:
-                    err = await ctx.send("Absolutely not!\nAre you trying to get me banned?")
-                    return
-
+                    embedVar = discord.Embed(title=":x: An exception has occurred", description="```py\nYou aren't allowed to use the n-word! ```", color=0xff0000)
+                    embedVar.set_footer(text="TOSBreakError")
+                    return await ctx.send(embed=embedVar)
+            
             def cleanup_code(content):
                 if content.startswith('```') and content.endswith('```'):
                     return '\n'.join(content.split('\n')[1:-1])
@@ -78,15 +88,19 @@ class _eval(commands.Cog):
             try:
                 exec(to_compile, env)
             except Exception as e:
-                err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-                return
+                err = f'```py\n{e} ```'
+                errtype = e.__class__.__name__
+                embedVar = discord.Embed(title=":x: An exception has occurred", description=f"{err}", color=0xff0000)
+                embedVar.set_footer(text=errtype)
+                return await ctx.send(embed=embedVar)
             func = env['func']
             try:
                 with redirect_stdout(stdout):
                     ret = await func()
             except Exception as e:
                 value = stdout.getvalue()
-                err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+                err = f'```py\n{value}{e} \n```'
+                errtype = e.__class__.__name__
             else:
                 value = stdout.getvalue()
                 if ret is None:
@@ -97,7 +111,7 @@ class _eval(commands.Cog):
                             paginated_text = paginate(value)
                             for page in paginated_text:
                                 if page == paginated_text[-1]:
-                                    out = await ctx.send(f'```py\n{page}\n```')
+                                    out = f'```py\n{page}\n```'
                                     break
                                 await ctx.send(f'```py\n{page}\n```')
                 else:
@@ -107,14 +121,23 @@ class _eval(commands.Cog):
                         paginated_text = paginate(f"{value}{ret}")
                         for page in paginated_text:
                             if page == paginated_text[-1]:
-                                out = await ctx.send(f'```py\n{page}\n```')
+                                out = f'```py\n{page}\n```'
                                 break
                             await ctx.send(f'```py\n{page}\n```')
-            # when finished remove the command message
+                if out == None:
+                  out = "Empty Response"
+                end = time.time() 
+                user = await self.bot.fetch_user(ctx.author.id)
+                embedVar = discord.Embed(title="Processing results", description=f"Processed in {math.trunc((end-start)*100)} ms", color=0x7289da)
+                embedVar.add_field(name="Input", value=f"```py\n{body}```", inline=False)
+                embedVar.add_field(name="Output", value=f"```py\n{message_c}```", inline=False)
+                return await user.send(embed=embedVar)
             if out:
-                return
+                await ctx.send(out)
             elif err:
-                return
+                embedVar = discord.Embed(title=":x: An exception has occurred", description=f"{err}", color=0xff0000)
+                embedVar.set_footer(text=errtype)
+                return await ctx.send(embed=embedVar)
             else:
                 return
         else:
